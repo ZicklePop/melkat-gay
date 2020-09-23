1522b5a45a156350cd42181a8b6521bd97dc389b
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2020-09-23 10:17:47 -0700

    react import codemod

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react';
 import Layout from '../components/layout'
 import TransPride from '../components/trans-pride'
 import LGBTQIAPride from '../components/lgbtqia-pride'
